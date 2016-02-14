c1fd2be7cb9d038adfbcf6f1152a40a81a03e87a
Tom Lei - Mac <lmd0209@msn.com>
2016-02-13 18:18:42 -0800

    added moment js and wrote more unit test covering formatdatetime and sanity check

--- spec/AWSSignatureTest.js
+++ spec/AWSSignatureTest.js
@@ -1,13 +1,96 @@
 "use strict";
 describe ("AWSSignature", () => {
 	var AWSSignature = require("../lib/AWSSignature");
-
+	var _ = require("lodash");
+	var moment = require("moment");
+	var signature;
+	beforeEach(() => {
+		signature = new AWSSignature();
+	});
 	describe("_reconstructQueryString", () => {
 		it("Should return string sorted by the params key",() => {
-			var signature = new AWSSignature();
 			var sorted = signature._reconstructQueryString('Param2=str2&Param1=str1');
 			expect(sorted).toEqual('Param1=str1&Param2=str2');
 		})
+	});
+
+	describe("sanityCheckRequiredForKeys", () => {
+		var keys = ['key1', 'key2'],
+			object = { 'key1': 'value1'};
+
+		it("should not raise exception where all the requred keys are present", () => {
+			var newObj = Object.assign({}, object, {'key2': 'value2'}),
+			sanityCheck = function() {
+				signature._sanityCheckRequiredKeysFor(newObj, keys);
+			};
+			expect(sanityCheck).not.toThrow();	
+		});
+
+		it("should raise exception where required keys are missing", () => {
+			var sanityCheck = function() {
+				signature._sanityCheckRequiredKeysFor(object, keys);
+			};
+			expect(sanityCheck).toThrow("Missing the following keys in options: key2");	
+		});
+
+		it("should raise exception when keys is not an array", () => {
+			var sanityCheck = function() {
+				signature._sanityCheckRequiredKeysFor(object, 'sdfs');
+			};
+			expect(sanityCheck).toThrow('second argument has to be an array');	
+		});
+
+		it("should raise exception when keys empty", () => {
+			var sanityCheck = function() {
+				signature._sanityCheckRequiredKeysFor(object, []);
+			};
+			expect(sanityCheck).toThrow('second argument cannot be empty');	
+		});
+
+		it("should raise exception when object is not an object", () => {
+			var sanityCheck = function() {
+				signature._sanityCheckRequiredKeysFor('sdfsdf', ['dsfds', 's']);
+			};
+			expect(sanityCheck).toThrow('first argument has to be a javascript object');	
+		});
+
+		it("should raise exception when object is empty", () => {
+			var sanityCheck = function() {
+				signature._sanityCheckRequiredKeysFor({}, ['dsfds', 's']);
+			};
+			expect(sanityCheck).toThrow('first argument cannot be an empty object');	
+		});
+	
+	});
+
+	describe("_formatDateTime", () => {
+		describe("should return Amazon style ISO-8601 datetime string", () => {
+			it("when given Amazon style ISO-8601 datetime string", () => {
+				let datetimeString = "20111015T080000Z";
+				expect(signature._formatDateTime(datetimeString)).toEqual(datetimeString);		
+			});
+
+			it("when given ISO-8601 datetime string", () => {
+				let datetimeString = "2015-10-15T08:00:00Z",
+					expectedString = "20151015T080000Z";
+				expect(signature._formatDateTime(datetimeString)).toEqual(expectedString);		
+			});
+
+			it("when given javascript Date object", () => {
+				let time = moment("2015-02-09T08:00:00Z"),
+					expectedString = "20150209T080000Z";
+				let formatedDateTime = time.toDate();
+				expect(signature._formatDateTime(formatedDateTime)).toEqual(expectedString);		
+			});
+		});
+
+		it("should raise exception when the string passed in is not recoginzable by momentjs", () => {
+			let formatDateTime = function() {
+				signature._formatDateTime('fake date');
+			}
+			expect(formatDateTime).toThrow('Unacceptable datetime string');		
+		});
+	
 	});
 
 	describe("When tested using Sample provided on AWS website", () => {
@@ -23,11 +106,9 @@ describe ("AWSSignature", () => {
 			body: ''
 		};
 
-		var signature,
-			secretKey = 'wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY';
+		var secretKey = 'wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY';
 
 		beforeEach(() => {
-			signature = new AWSSignature();
 			signature.setParams(options);
 		});
 
